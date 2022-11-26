ae47aa9be35b963dbdab5bd87304606d861c075f
Abdulkarim6 <mdhossainjwel687@gmail.com>
2022-11-26 22:10:51 +0600

    advertise product data load client side and send database

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run() {
         const sellerProductsCollection = client.db('cheapLaptops').collection('sellerProducts');
         const usersCollection = client.db('cheapLaptops').collection('users');
         const buyerBookingProductsCollection = client.db('cheapLaptops').collection('buyerBookingProducts');
+        const advertisedProductsCollection = client.db('cheapLaptops').collection('advertisedProducts');
 
 
         /* get categories data and send client side */
@@ -39,14 +40,14 @@ async function run() {
             res.send(booking)
         });
 
-        /* send created user to database */
+        /* send client side created users to database */
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result)
         });
 
-        /* post client appointment booking to database */
+        /* Load Buyer booking product from client side and send database[bookingModal] */
         app.post('/buyerBookingProducts', async (req, res) => {
             const buyerBookingProduct = req.body;
             const result = await buyerBookingProductsCollection.insertOne(buyerBookingProduct)
@@ -54,7 +55,7 @@ async function run() {
 
         });
 
-        /* get client bookings and show client side */
+        /* get every seller products collection from database and send client side[MyProducts] */
         app.get('/products', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
@@ -77,13 +78,28 @@ async function run() {
             res.send(result)
         });
 
-        /* deleted Product operation from client side */
+        /*create this api for deleted Product operation from client side */
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await sellerProductsCollection.deleteOne(query)
             res.send(result)
-        })
+        });
+
+        /* Advertised Product load from client side and send database */
+        app.post('/advertiseProduct', async (req, res) => {
+            const product = req.body;
+            // console.log(product);
+            const result = await advertisedProductsCollection.insertOne(product);
+            res.send(result)
+        });
+
+        /* get Advertised products from database and send client side */
+        app.get('/advertiseProducts', async (req, res) => {
+            const query = {};
+            const advertiseProducts = await advertisedProductsCollection.find(query).toArray();
+            res.send(advertiseProducts)
+        });
 
 
     }
